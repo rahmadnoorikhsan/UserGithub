86ca198f628849d11adae62e1e57004658de5c6f
Rahmad Noor Ikhsan <rajaikhsan2021@gmail.com>
2023-03-09 08:39:38 +0700

    Initial commit

---
+++ app/src/main/java/com/ikhsan/submissionusergithub/ApiService.kt
@@ -0,0 +1,4 @@
+package com.ikhsan.submissionusergithub
+
+class ApiService {
+}
---
+++ app/src/main/java/com/ikhsan/submissionusergithub/UserResponse.kt
@@ -0,0 +1,15 @@
+package com.ikhsan.submissionusergithub
+
+import com.google.gson.annotations.SerializedName
+
+data class UserResponse(
+
+	@field:SerializedName("total_count")
+	val totalCount: Int,
+
+	@field:SerializedName("incomplete_results")
+	val incompleteResults: Boolean,
+
+	@field:SerializedName("items")
+	val items: List<Any>
+)
