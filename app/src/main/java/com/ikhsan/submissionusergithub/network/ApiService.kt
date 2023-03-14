83e684565e8dc55c5acdd51d5225592b1f2473fb
Rahmad Noor Ikhsan <117014156+rahmadnoorikhsan@users.noreply.github.com>
2023-03-14 15:53:36 +0700

    Add files via upload

--- app/src/main/java/com/ikhsan/submissionusergithub/network/ApiService.kt
+++ app/src/main/java/com/ikhsan/submissionusergithub/network/ApiService.kt
@@ -1,1 +1,29 @@
+package com.ikhsan.submissionusergithub.network
 
+import com.ikhsan.submissionusergithub.response.DetailResponse
+import com.ikhsan.submissionusergithub.response.ListUserResponse
+import com.ikhsan.submissionusergithub.response.UserResponseItem
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+    @GET("search/users")
+    fun getSearchUsers(
+        @Query("q") query: String
+    ): Call<ListUserResponse>
+
+    @GET("users/{username}")
+    fun getDetailUsers(
+        @Path("username") username: String
+    ): Call<DetailResponse>
+
+    @GET("users/{username}/followers")
+    fun getFollowers(
+        @Path("username") username: String
+    ): Call<List<UserResponseItem>>
+
+    @GET("users/{username}/following")
+    fun getFollowing(
+        @Path("username") username: String
+    ): Call<List<UserResponseItem>>
+}
